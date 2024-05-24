7bda7c7472e1ed2fe67c9b5e90a49261c740b3e1
duddn2012 <gowoo123@naver.com>
2024-05-24 12:53:45 +0900

    feat: account number generator

---
+++ src/main/java/com/project/hana_piece/account/constant/AccountNumberGenerator.java
@@ -0,0 +1,22 @@
+package com.project.hana_piece.account.constant;
+
+import java.util.Random;
+
+public class AccountNumberGenerator {
+
+    private static final Random RANDOM = new Random();
+    private static final Integer PREFIX = 178;
+    private static final int LENGTH_WITHOUT_PREFIX = 11;
+
+    public static String generateAccountNumber() {
+        StringBuilder result = new StringBuilder();
+
+        result.append(PREFIX);
+        for (int i = 0; i < LENGTH_WITHOUT_PREFIX; i++) {
+            int digit = RANDOM.nextInt(10);
+            result.append(digit);
+        }
+
+        return result.toString();
+    }
+}
--- src/main/java/com/project/hana_piece/account/service/AccountService.java
+++ src/main/java/com/project/hana_piece/account/service/AccountService.java
@@ -1,7 +1,6 @@
 package com.project.hana_piece.account.service;
 
-import static com.project.hana_piece.account.constant.AccountNumberConst.LENGTH_WITHOUT_PREFIX;
-import static com.project.hana_piece.account.constant.AccountNumberConst.PREFIX;
+import static com.project.hana_piece.account.constant.AccountNumberGenerator.generateAccountNumber;
 
 import com.project.hana_piece.account.domain.Account;
 import com.project.hana_piece.account.domain.AccountType;
@@ -10,7 +9,6 @@ import com.project.hana_piece.account.repository.AccountRepository;
 import com.project.hana_piece.user.domain.User;
 import com.project.hana_piece.user.exception.UserNotFoundException;
 import com.project.hana_piece.user.repository.UserRepository;
-import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -29,18 +27,5 @@ public class AccountService {
             Account.builder().user(user).accountNumber(generateAccountNumber()).accountType(AccountType.CHECKING).balance(0L).build());
 
         return AccountUpsertResponse.fromEntity(savedAccount);
-    }
-    
-    public String generateAccountNumber() {
-        Random RANDOM = new Random();
-        StringBuilder result = new StringBuilder();
-
-        result.append(PREFIX.getProperty());
-        for (int i = 0; i < LENGTH_WITHOUT_PREFIX.getProperty(); i++) {
-            int digit = RANDOM.nextInt(10);
-            result.append(digit);
-        }
-
-        return result.toString();
     }
 }

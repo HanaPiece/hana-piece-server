970048bd4762431ef84ebe2a84004d5decfb2c1c
duddn2012 <gowoo123@naver.com>
2024-05-24 11:50:24 +0900

    feat: account number generate strategy

--- src/main/java/com/project/hana_piece/account/repository/AccountRepository.java
+++ src/main/java/com/project/hana_piece/account/repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package com.project.hana_piece.account.repository;
 
 import com.project.hana_piece.account.domain.Account;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    Optional<Account> findByAccountNumber(String result);
 }

45a59f8373f2a02ab6136be8067ddceede64e5d0
jungjaegun <jregun@naver.com>
2024-05-29 09:56:51 +0900

    feat/car get api

--- src/main/java/com/project/hana_piece/goal/controller/UserGoalController.java
+++ src/main/java/com/project/hana_piece/goal/controller/UserGoalController.java
@@ -1,10 +1,11 @@
 package com.project.hana_piece.goal.controller;
 
-import com.project.hana_piece.ai.service.AiService;
 import com.project.hana_piece.goal.dto.ApartmentGetResponse;
 import com.project.hana_piece.goal.dto.ApartmentPricePredictRequest;
 import com.project.hana_piece.goal.dto.ApartmentPricePredictResponse;
+import com.project.hana_piece.goal.dto.CarGetResponse;
 import com.project.hana_piece.goal.service.ApartmentService;
+import com.project.hana_piece.goal.service.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ import java.util.List;
 public class UserGoalController {
 
     private final ApartmentService apartmentService;
-    private final AiService aiService;
+    private final CarService carService;
 
     @GetMapping("/apartments")
     public ResponseEntity<List<ApartmentGetResponse>> getAllApartments() {
@@ -28,6 +29,12 @@ public class UserGoalController {
     @PostMapping("/apartments/predict")
     public ResponseEntity<ApartmentPricePredictResponse> predictApartmentPrice(@RequestBody ApartmentPricePredictRequest request) {
         ApartmentPricePredictResponse response = apartmentService.predictApartmentPrice(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/cars")
+    public ResponseEntity<List<CarGetResponse>> getAllCars() {
+        List<CarGetResponse> response = carService.getAllCars();
         return ResponseEntity.ok(response);
     }
 }

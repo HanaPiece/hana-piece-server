94427d4e9bab4e0b0a8bf98417eebd366b272fc6
고영우 <31121731+duddn2012@users.noreply.github.com>
2024-06-03 09:34:16 +0900

    conflict solve

--- src/main/java/com/project/hana_piece/product/service/ProductService.java
+++ src/main/java/com/project/hana_piece/product/service/ProductService.java
@@ -51,7 +51,6 @@ public class ProductService {
                 .map(enrolledProduct -> enrolledProduct.getProduct().getProductId())
                 .toList();
 
-        GeminiCallResponse aiResponse =  aiService.callGenerativeLanguageApi(geminiPrompt);
         // json 파싱
         // 응답
         // 추천 상품 목록 생성
